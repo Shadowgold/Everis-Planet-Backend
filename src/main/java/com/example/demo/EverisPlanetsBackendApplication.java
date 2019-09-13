ccd557c5c0a5fb0a4395383886ee56feabce232a
Shadowgold <agustin.espinoza.21@gmail.com>
2019-09-13 14:55:18 -0300

    Primer commit - Creación del proyecto - Packages

---
+++ src/main/java/com/example/demo/EverisPlanetsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverisPlanetsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverisPlanetsBackendApplication.class, args);
+	}
+
+}
