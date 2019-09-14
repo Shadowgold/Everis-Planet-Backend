bf80ab325b3d7bf50aba6ef08171033fe11da979
Shadowgold <agustin.espinoza.21@gmail.com>
2019-09-14 13:01:02 -0300

    Entidades terminadas y properties

--- src/main/java/com/example/demo/Entity/Planet.java
+++ src/main/java/com/example/demo/Entity/Planet.java
@@ -1,5 +1,68 @@
 package com.example.demo.Entity;
 
-public class Planet {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "api_planet")
+public class Planet extends EntityBean {
+	
+	//Attributes
+	@Column(name = "planet_name")
+	private String name;
+	
+	@Column(name = "planet_size")
+	private int size;
+
+	@ManyToOne(cascade = CascadeType.REMOVE)
+	@JoinColumn(name = "star_fk_planet")
+	private Star star;
+
+	
+	
+	//Constructors
+	
+	public Planet() {
+	}
+
+	
+	public Planet(String name, int size) {
+		this.name = name;
+		this.size = size;
+	}
+
+
+	//Getters & Setters
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public Star getStar() {
+		return star;
+	}
+
+	public void setStar(Star star) {
+		this.star = star;
+	}
+	
+	
 
 }
--- src/main/java/com/example/demo/Entity/Star.java
+++ src/main/java/com/example/demo/Entity/Star.java
@@ -1,5 +1,71 @@
 package com.example.demo.Entity;
 
-public class Star {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "api_star")
+public class Star extends EntityBean{
+	
+	//Attributes
+	@Column(name = "star_name")
+	private String name;
+	
+	@Column(name = "star_density")
+	private int density;
+	
+	@OneToMany(mappedBy = "star")
+	private List<Planet> planets = new ArrayList();
+
+	
+	
+	//Constructors
+	
+	public Star() {
+	}
+	
+	
+
+	public Star(String name, int density) {
+		this.name = name;
+		this.density = density;
+	}
+
+
+
+	//Getters & Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDensity() {
+		return density;
+	}
+
+	public void setDensity(int density) {
+		this.density = density;
+	}
+
+	public List<Planet> getPlanets() {
+		return planets;
+	}
+
+	public void setPlanets(List<Planet> planets) {
+		this.planets = planets;
+	}
+	
+	
 
 }
