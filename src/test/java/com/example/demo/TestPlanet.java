3370ddfce38d184bbee2cfc0de0034c20fd24e9e
Shadowgold <agustin.espinoza.21@gmail.com>
2019-09-20 15:30:18 -0300

    Agrego Pruebas Unitarias para Planet y Star

--- src/test/java/com/example/demo/TestPlanet.java
+++ src/test/java/com/example/demo/TestPlanet.java
@@ -1,5 +1,80 @@
 package com.example.demo;
 
-public class TestPlanet {
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+import com.example.demo.Entity.Planet;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Agustin
+ */
+public class TestPlanet {
+    
+    static Planet instance;
+    public TestPlanet() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        instance = new Planet();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        
+        String expResult = "Tierra";
+        instance.setName("Tierra");
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        
+    }
+
+    @Test
+    public void testSetName() {
+        System.out.println("setNombre");
+        String nombre = "Marte";
+        String expResult="Marte";
+        instance.setName(nombre);
+        assertEquals(expResult, instance.getName());
+    }
+
+    @Test
+    public void testGetSize() {
+        System.out.println("getSize");
+        
+        int expResult = 5;
+        instance.setSize(5);
+        int result = instance.getSize();
+        assertEquals(expResult, result);
+        
+    }
+
+    @Test
+    public void testSetSize() {
+        System.out.println("setSize");
+        int tamaño = 500;
+        int expResult =500;
+        instance.setSize(tamaño);
+        assertEquals(expResult, instance.getSize());
+    }
+    
 }
--- src/test/java/com/example/demo/TestStar.java
+++ src/test/java/com/example/demo/TestStar.java
@@ -1,5 +1,80 @@
 package com.example.demo;
 
-public class TestStar {
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+import com.example.demo.Entity.Star;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Agustin
+ */
+public class TestStar {
+    
+    static Star instance;
+    public TestStar() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        instance = new Star();
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        
+        String expResult = "Sol";
+        instance.setName("Sol");
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        
+    }
+
+    @Test
+    public void testSetName() {
+        System.out.println("setNombre");
+        String nombre = "Alfa Centauri";
+        String expResult="Alfa Centauri";
+        instance.setName(nombre);
+        assertEquals(expResult, instance.getName());
+    }
+
+    @Test
+    public void testGetDensity() {
+        System.out.println("getDensity");
+        
+        int expResult = 5;
+        instance.setDensity(5);
+        int result = instance.getDensity();
+        assertEquals(expResult, result);
+        
+    }
+
+    @Test
+    public void testSetDensity() {
+        System.out.println("setDensity");
+        int densidad = 500;
+        int expResult =500;
+        instance.setDensity(densidad);
+        assertEquals(expResult, instance.getDensity());
+    }
+    
 }
